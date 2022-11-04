cbc00ae1f52878efe79138e2e634f800262b5f61
MoonShinkiro <cmonmcfly@gmail.com>
2022-11-04 19:36:38 -0400

    initial commit

---
+++ slash/loop.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+
+module.exports = {
+    data: new SlashCommandBuilder().setName("loop").setDescription("Loops the current song, choices between '**on**' and '**off**' only.")
+    .addStringOption((option) => option.setName("mode").setDescription("Set the mode between **on** or **off** only.").setRequired(true)),
+    run: async ({ client, interaction }) => {
+        let mode = null
+        const queue = client.player.getQueue(interaction.guildId)
+
+        if (!queue) {
+            await interaction.editReply("No songs in queue to loop")
+        }
+        else {
+            switch (interaction.options.getString("mode")) {
+                case 'off':
+                    mode = 0
+                    break
+                case 'on':
+                    mode = 1
+                    break
+            }
+            mode = queue.setRepeatMode(mode)
+            mode = mode == 1 ? 'on' : 'off'
+            await interaction.editReply(`Song loop has successfully changed.`)
+        }
+    }
+}
+
